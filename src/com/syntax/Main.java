4d4c6b61fd5506d0a1f79c9a2d6b3247affea210
gabri <bird.gs88@gmail.com>
2021-12-15 18:57:46 -0500

    Initial commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,13 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+
+        for (int i=0; i<5; i++){
+            System.out.println("Batch10 is great");
+            Thread.sleep(2000);
+        }
+    }
+}
