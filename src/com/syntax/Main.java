d667d75c4c21a051a59ddd5481224918f2bf4c00
gabri <bird.gs88@gmail.com>
2021-12-15 19:46:13 -0500

    Second Commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 	// write your code here
 
-        for (int i=0; i<5; i++){
+        for (int i=0; i<15; i++){
             System.out.println("Batch10 is great");
             Thread.sleep(2000);
         }
